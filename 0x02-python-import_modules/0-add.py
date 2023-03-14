dcf15851f2466ba69a9cfb125ce2347f559b45ec
paulagbelex <paulagbelex@gmail.com>
2023-03-14 09:31:53 -0700

    made few corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    som = a + b
+    som = add(a, b)
     print("{} + {} = {}".format(a, b, som))
