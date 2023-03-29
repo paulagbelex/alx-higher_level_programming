78a9480dc5a01cb02740159e3a0cd01a24d4bb6b
paulagbelex <paulagbelex@gmail.com>
2023-03-29 11:33:26 -0700

    print integer safely and made code correction on 0-safe-print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-        try:
-                sublist = my_list[:x]
-
-                for element in sublist:
-                        print(element, end=' ')
-
-                print()
-                return len(sublist)
-        except:
-                print("An error occurred")
-                return 0
+        ret = 0
+        for i in range(x):
+                try:
+                        print("{}".format(my_list[i]), end="")
+                        ret += 1
+                except IndexError:
+                        break
+        print("")
+        return (ret)
